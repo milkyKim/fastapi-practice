a087d8b12149a4033e1502e2df892bed83766699
milkyKim <kimminji122258@gmail.com>
2023-05-15 00:53:50 +0900

    #0514 question 스키마 생성

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from models import Question
+from domain.question import question_schema
 
 router = APIRouter(
     prefix="/api/question",
 )
 
 
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
     return _question_list
