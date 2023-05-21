d1add6a33453cca6a066a90cba12a9a4387f4a07
milkyKim <kimminji122258@gmail.com>
2023-05-21 13:10:51 +0900

    #0521 3-07 회원가입 기능 구현

---
+++ domain/user/user_schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, validator, EmailStr
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    password: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    username: str
+    password1: str
+    password2: str
+    email: EmailStr
+
+    @validator("username", "password1", "password2", "email")
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("빈 값은 허용되지 않습니다.")
+        return v
+
+    @validator("password2")
+    def passwords_match(cls, v, values):
+        if "password1" in values and v != values["password1"]:
+            raise ValueError("비밀번호가 일치하지 않습니다")
+        return v
